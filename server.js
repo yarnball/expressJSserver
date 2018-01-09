266cf35047384b0fd8a91939471194f39db7860f
user1 <user1@user1s-MacBook-Pro.local>
2018-01-09 16:15:27 +1100

    Added body parser, so it can ready values posted from forms

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const bodyParser= require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
+  // GET THE HOME PAGE
   res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/api', (req, res) => {
-  res.send({allTheData:'genius'})
+  res.send({allTheData:'geneeius'})
 })
 
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!', req.body)
+})
 
 app.listen(3005, function() {
   console.log('Express.js server live on 3005')
